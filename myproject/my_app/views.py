5140959277f69cac141f7ec30b210a44ccf38cd2
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-04-10 17:09:14 +0500

    sending dynamic data to template files

--- myproject/my_app/views.py
+++ myproject/my_app/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("<h1>Hey welcome</h1>")
-    return render(request,"index.html")
+    
+    # return render(request,"index.html")
+    #sending dynamic data 
+    context={
+        'name':'Ubaid',
+        'age' : 23,
+        'degree':'BSCS'
+    }
+    
+    return render(request,'index.html',context)
